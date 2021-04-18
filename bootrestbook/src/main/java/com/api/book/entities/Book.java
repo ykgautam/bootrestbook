d8fd2bfbff2e1edb42d3b2c77affa53286db9e9d
Yash <yk.gautam97@gmail.com>
2021-04-18 22:54:56 +0530

    connected with db using jpa CRUD operation

--- bootrestbook/src/main/java/com/api/book/entities/Book.java
+++ bootrestbook/src/main/java/com/api/book/entities/Book.java
@@ -1,6 +1,17 @@
 package com.api.book.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
 public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String title;
 	private String author;
--- bootrestbook/src/main/java/com/api/book/services/BookService.java
+++ bootrestbook/src/main/java/com/api/book/services/BookService.java
@@ -1,36 +1,37 @@
 package com.api.book.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.api.book.dao.BookRepository;
 import com.api.book.entities.Book;
 
 //@Service
 @Component
 public class BookService {
-
-	private static List<Book> bookList = new ArrayList<Book>();
-
-	static {
-		bookList.add(new Book(2, "spring reference", "ankit"));
-		bookList.add(new Book(1, "java reference", "yash"));
-		bookList.add(new Book(23, "head first to java", "omkar"));
-	}
+	/*
+	 * private static List<Book> bookList = new ArrayList<Book>(); static {
+	 * bookList.add(new Book(2, "spring reference", "ankit")); bookList.add(new
+	 * Book(1, "java reference", "yash")); bookList.add(new Book(23,
+	 * "head first to java", "omkar")); }
+	 */
+	@Autowired
+	private BookRepository bookRepository;
 
 	public List<Book> getAllBooks() {
-		return bookList;
+		List<Book> list = (List<Book>) this.bookRepository.findAll();
+		return list;
 	}
 
 	public Book getBookById(int id) {
 //		using stream api to get single book wrt id
 		Book book = null;
 		try {
-			book = bookList.stream().filter(e -> e.getId() == id).findFirst().get();
-
+//			book = bookList.stream().filter(e -> e.getId() == id).findFirst().get();
+			book = this.bookRepository.findById(id);
 		} catch (NoSuchElementException e2) {
 			System.out.println("book id not matched..........");
 			e2.printStackTrace();
@@ -40,24 +41,26 @@ public class BookService {
 
 //	adding the book info
 	public Book addBook(Book b) {
-		bookList.add(b);
-		return b;
+//		bookList.add(b);
+		Book result = this.bookRepository.save(b);
+		return result;
 	}
 
 //	delete a single book
 	public void deleteBook(int id) {
-		bookList = bookList.stream().filter(book -> book.getId() != id).collect(Collectors.toList());
+//		bookList = bookList.stream().filter(book -> book.getId() != id).collect(Collectors.toList());
+		this.bookRepository.deleteById(id);
 	}
 
 //	update book
 	public void updateBook(Book book, int id) {
-		bookList.stream().map(b -> {
-			if (b.getId() == id) {
-				b.setTitle(book.getTitle());
-				b.setAuthor(book.getAuthor());
-			}
-			return b;
-		}).collect(Collectors.toList());
+		/*
+		 * bookList.stream().map(b -> { if (b.getId() == id) {
+		 * b.setTitle(book.getTitle()); b.setAuthor(book.getAuthor()); } return b;
+		 * }).collect(Collectors.toList());
+		 */
+		book.setId(id);
+		this.bookRepository.save(book);
 	}
 
 }
