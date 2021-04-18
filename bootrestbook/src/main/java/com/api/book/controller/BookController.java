0291e03d30120fefe430a8435c5500cac85f902f
Yash <yk.gautam97@gmail.com>
2021-04-18 15:46:46 +0530

    added CRUD operation for books @DeleteMapping ,@PostMapping ,@PutMapping

--- bootrestbook/src/main/java/com/api/book/controller/BookController.java
+++ bootrestbook/src/main/java/com/api/book/controller/BookController.java
@@ -3,9 +3,11 @@ package com.api.book.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,28 +20,40 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 
+//	get single book handler
 //	@RequestMapping(value = "/books", method = RequestMethod.GET)
 	@GetMapping(value = "/books")
 	public List<Book> getBooks() {
-		/*
-		 * Book book = new Book(); book.setId(1); book.setTitle("Java notes");
-		 * book.setAuthor("yash");
-		 */
 
 		System.out.println("this is getbooks on console ");
 		return (List<Book>) this.bookService.getAllBooks();
 	}
 
+//	get single book handler
 	@GetMapping(value = "/books/{id}")
 	public Book getBook(@PathVariable("id") int id) {
 		return this.bookService.getBookById(id);
 	}
 
+//	add single book handler
 	@PostMapping(value = "/books")
 	public Book addBook(@RequestBody Book book) {
-		Book book2 = this.bookService.addBook(book);
-		System.out.println(book2);
-		return book2;
+		Book newBook = this.bookService.addBook(book);
+		System.out.println(newBook);
+		return newBook;
+	}
+
+//	delete single book handler
+	@DeleteMapping(value = "books/{id}")
+	public void deleteBook(@PathVariable("id") int id) {
+		this.bookService.deleteBook(id);
+	}
+
+//	update book handler
+	@PutMapping(value = "/books/{id}")
+	public Book updateBook(@RequestBody Book book, @PathVariable("id") int id) {
+		this.bookService.updateBook(book, id);
+		return book;
 	}
 
 }
--- bootrestbook/src/main/java/com/api/book/services/BookService.java
+++ bootrestbook/src/main/java/com/api/book/services/BookService.java
@@ -2,6 +2,7 @@ package com.api.book.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
@@ -35,6 +36,22 @@ public class BookService {
 	public Book addBook(Book b) {
 		bookList.add(b);
 		return b;
+	}
+
+//	delete a single book
+	public void deleteBook(int id) {
+		bookList = bookList.stream().filter(book -> book.getId() != id).collect(Collectors.toList());
+	}
+
+//	update book
+	public void updateBook(Book book, int id) {
+		bookList.stream().map(b -> {
+			if (b.getId() == id) {
+				b.setTitle(book.getTitle());
+				b.setAuthor(book.getAuthor());
+			}
+			return b;
+		}).collect(Collectors.toList());
 	}
 
 }
